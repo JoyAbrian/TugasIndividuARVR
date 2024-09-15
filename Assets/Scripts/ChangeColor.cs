0548523a8efeb9a2a46521cac7ed7b878c2013f3
anisanhy <joyabrianrantepasang14@gmail.com>
2024-09-15 17:35:50 +0800

    commit tugasss

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    [SerializeField] private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    public void ColorChange()
+    {
+        GetComponent<Renderer>().material = material;
+    }
+}
---
+++ Assets/Scripts/RemoveObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveObject : MonoBehaviour
+{
+    public int jarakRaycast = 20;
+    private RaycastHit raycastHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        if (Physics.Raycast(ray, out raycastHit, jarakRaycast))
+        {
+            if (raycastHit.transform.CompareTag("Obstacle"))
+            {
+                Destroy(raycastHit.transform.gameObject);
+            }
+        }
+    }
+}
